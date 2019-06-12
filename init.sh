7944fd9495fd4763d9c5f56a7cc0042283eb07e8
jhwang <jhwang@paas.local>
2019-06-12 16:45:32 +0900

    .

--- init.sh
+++ init.sh
@@ -13,7 +13,6 @@ sudo mv terraform /usr/local/bin
 
 ## pivnet
 NAME=pivnet
-<<<<<<< HEAD
 wget $(curl -s https://api.github.com/repos/pivotal-cf/pivnet-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && \
 chmod a+x ${NAME} && \
 sudo mv ${NAME} /usr/local/bin
@@ -29,13 +28,6 @@ NAME=bosh
 wget $(curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && \
 chmod a+x ${NAME} && \
 sudo mv ${NAME} /usr/local/bin
-=======
-wget $(curl -s https://api.github.com/repos/pivotal-cf/pivnet-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && chmod a+x ${NAME} && sudo mv ${NAME} /usr/local/bin
-
-## om
-NAME=om
-wget $(curl -s https://api.github.com/repos/pivotal-cf/om/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && chmod a+x ${NAME} && sudo mv ${NAME} /usr/local/bin
->>>>>>> fb4bd70f8f6ff1be6c687950bb67a042cd92883c
 
 ## bosh
 NAME=bosh
