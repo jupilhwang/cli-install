4f27b8b8141b9b3b4580d4407da7bdcd9a2ea007
jhwang <jhwang@paas.local>
2019-06-12 16:29:54 +0900

    .

--- init.sh
+++ init.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-sudo apt -y update && sudo apt -y upgrade 
-sudo apt -y install unzip jq build-essential ruby-dev && gem install --no-ri --no-rdoc cf-uaac
+sudo apt -y update && sudo apt -y upgrade && \
+sudo apt -y install unzip jq build-essential ruby-dev python3 python3-pip &&\
+gem install --no-ri --no-rdoc cf-uaac
 
+## terraform 
+VERSION=0.11.14
+wget -O terraform.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip && \
+unzip terraform.zip && \
+sudo mv terraform /usr/local/bin
 
 ## pivnet
-wget $(curl -s https://api.github.com/repos/pivotal-cf/pivnet-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O pivnet && chmod a+x pivnet && sudo mv pivnet /usr/local/bin
+NAME=pivnet
+wget $(curl -s https://api.github.com/repos/pivotal-cf/pivnet-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && \
+chmod a+x ${NAME} && \
+sudo mv ${NAME} /usr/local/bin
 
 ## om
-wget $(curl -s https://api.github.com/repos/pivotal-cf/om/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O om && chmod a+x om && sudo mv om /usr/local/bin
+NAME=om
+wget $(curl -s https://api.github.com/repos/pivotal-cf/om/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && \
+chmod a+x ${NAME} && \
+sudo mv ${NAME} /usr/local/bin
+
+## bosh
+NAME=bosh
+wget $(curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && \
+chmod a+x ${NAME} && \
+sudo mv ${NAME} /usr/local/bin
 
 
 
