4875de14e7a91c743b0db9ce6edea511f4a92c9d
jhwang <jhwang@paas.local>
2019-06-14 10:12:29 +0900

    .

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ sudo apt -y update && sudo apt -y upgrade && \
 sudo apt -y install unzip jq build-essential ruby-dev python3 python3-pip &&\
 gem install --no-ri --no-rdoc cf-uaac
 
-
 ## terraform 
 VERSION=0.11.14
 wget -O terraform.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip && \
@@ -25,13 +24,9 @@ sudo mv ${NAME} /usr/local/bin
 
 ## bosh
 NAME=bosh
-wget $(curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && \
+wget $(c url -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && \
 chmod a+x ${NAME} && \
 sudo mv ${NAME} /usr/local/bin
-
-## bosh
-NAME=bosh
-wget $(curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O ${NAME} && chmod a+x ${NAME} && sudo mv ${NAME} /usr/local/bin
 
 # fly
 NAME=fly
@@ -39,7 +34,9 @@ wget $(curl -s https://api.github.com/repos/concourse/concourse/releases/latest 
 
 # cf
 NAME=cf
-wget https://packages.cloudfoundry.org/stable?release=linux64-binary&version=$(curl -s https://api.github.com/repos/cloudfoundry/cli/releases/latest | jq -r '.name' | sed '/v//g/')&source=github-rel -O- | tar xzvf - -C /tmp && chmod a+x /tmp/${NAME} && sudo mv /tmp/${NAME} /usr/local/bin
+wget "https://packages.cloudfoundry.org/stable?release=linux64-binary&version=$(curl -s https://api.github.com/repos/cloudfoundry/cli/releases/latest | jq -r '.name' | sed 's/v//g')&source=github-rel" -O- | tar xzvf - -C /tmp && chmod a+x /tmp/${NAME} && sudo mv /tmp/${NAME} /usr/local/bin
+
 # pks
+
 # kubectl
 
