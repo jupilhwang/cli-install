39db67faa7b93305b59e69c5326c88257af5ec35
jupilhwang <jupil.hwang@gmail.com>
2020-02-15 01:48:43 -0800

    credhub

--- init.sh
+++ init.sh
@@ -26,17 +26,17 @@ wget $(curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases/lates
 chmod a+x ${NAME} && \
 sudo mv ${NAME} /usr/local/bin
 
-## fly
-NAME=fly
-wget $(curl -s https://api.github.com/repos/concourse/concourse/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | select(.name | contains("fly")) | select(.name | contains("sha") | not) | .browser_download_url') -O- | tar xzvf - -C /tmp && chmod a+x /tmp/${NAME} && sudo mv /tmp/${NAME} /usr/local/bin
-
 ## cf
 NAME=cf
 wget "https://packages.cloudfoundry.org/stable?release=linux64-binary&version=$(curl -s https://api.github.com/repos/cloudfoundry/cli/releases/latest | jq -r '.name' | sed 's/v//g')&source=github-rel" -O- | tar xzvf - -C /tmp && chmod a+x /tmp/${NAME} && sudo mv /tmp/${NAME} /usr/local/bin
 
+## fly
+NAME=fly
+wget $(curl -s https://api.github.com/repos/concourse/concourse/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | select(.name | contains("fly")) | select(.name | contains("sha") | not) | .browser_download_url') -O- | tar xzvf - -C /tmp && chmod a+x /tmp/${NAME} && sudo mv /tmp/${NAME} /usr/local/bin
+
 ## credhub
 NAME=credhub
-wget $(curl -s https://api.github.com/repos/cloudfoundry-incubator/credhub-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O- | tar xzf - -C /tmp && chmod a+x /tmp/${NAME} && sudo mv /tmp/${NAME} /usr/local/bin
+wget $(curl -s https://api.github.com/repos/cloudfoundry-incubator/credhub-cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url') -O- | sudo tar xzvfp - -C /tmp && sudo chmod a+x /tmp/${NAME} && sudo mv /tmp/${NAME} /usr/local/bin
 
 ## uaa-cli
 NAME=uaac
