39458604976291195dcb00caa2db38ca654fbb4b
jupilhwang <jupil.hwang@gmail.com>
2020-06-25 20:48:16 +0900

    a

--- init.sh
+++ init.sh
@@ -10,11 +10,36 @@ fi
 sudo apt -y update && sudo apt -y upgrade && sudo apt -y install \
 apt-transport-https ca-certificates curl gnupg-agent software-properties-common unzip jq build-essential python3 python3-pip python3-venv docker.io
 
+## Enable sudo
+sudo tee /etc/sudoers.d/${USER} >/dev/null <<EOF
+${USER} ALL=(ALL) NOPASSWD:ALL
+EOF
+
+## Add user to docker group
+sudo usermod -G docker ${USER}
+
+## Docker service enable
+sudo systemctl enable docker
+sudo systemctl start docker
+
 ## Disable swap
 sudo swapoff --all
 sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 sudo ufw Disable	# Do not this in producton
 echo "Hosekeeping done"
+
+## Configure Docker release
+sudo tee /etc/docker/daemon.json >/dev/null <<EOF
+{
+	"exec-opts": ["native.cgroupdriver=systemd"],
+	"insecure-registries": ["0.0.0.0/0"],
+	"log-driver": "json-file",
+	"log-opts": {}
+		"max-size": "100m"
+	},
+	"storage-driver": "overlay2"
+}
+EOF
 
 ## fly
 echo "### Downloading Fly cli for concourse"
@@ -30,6 +55,10 @@ wget -q $(curl -s https://api.github.com/repos/cloudfoundry-incubator/uaa-cli/re
 echo "### Downloading kubectl"
 NAME=kubectl
 curl -sLO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && chmod a+x kubectl && sudo mv kubectl /usr/local/bin
+
+echo "alias k=kubectl" >> .bashrc
+echo "source <(kubectl completion bash)" >> .bashrc
+echo "source <(kubectl completion bash | sed 's/kubectl/k/g')" >> .bashrc
 
 ## Velero
 echo "### Downloading Velero"
@@ -54,8 +83,20 @@ wget -q $(curl -s https://api.github.com/repos/vmware-tanzu/octant/releases/late
 #echo "### Downloading tkg"
 
 ######
+echo "### Downloading govc"
 NAME=govc
 wget -q $(curl -s https://api.github.com/repos/vmware/govmomi/releases/latest | jq -r '.assets[] | select(.name | contains("linux") and contains("amd64")) | .browser_download_url') && gzip -d ${NAME}*gz && chmod a+x ${NAME}_linux_amd64 && sudo mv ${NAME}_linux_amd64 /usr/local/bin/${NAME}
+
+tee ~/.govc-env >/dev/null <<EOF
+export GOVC_INSECURE=1
+export GOVC_URL=https://vcsa-01.haas-242.pez.pivotal.io/sdk
+export GOVC_USERNAME=administrator@vsphere.local
+export GOVC_PASSWORD=VMware1!
+export GOVC_DATASTORE=LUN01
+export GOVC_NETWORK="VM Network"
+EOF
+
+echo "source .govc-env" >> ~/.bashrc
 
 ##
 ## cli versions
