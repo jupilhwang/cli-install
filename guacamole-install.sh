190c475ddad793b3aa9f62b00dc482a64c1a5a3b
jupilhwang <jupil.hwang@gmail.com>
2020-12-22 13:34:51 +0000

    install guacamole

---
+++ guacamole-install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+GUACAMOLE_VERSION=1.2.0
+GUACAMOLE_FQDN=jumpbox.kr.pivotal.io
+ADMIN_PASSWORD=$(echo -n P@ssw0rd | openssl md5 | sed 's/(stdin)= //g')
+
+#set -eo pipefail
+
+sudo apt -y update && sudo apt -y upgrade && \
+	sudo apt -y install tomcat9 tomcat9-admin tomcat9-user nginx certbot python3-certbot-nginx make gcc g++ libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libssl-dev libvorbis-dev libwebp-dev
+
+sudo systemctl enable tomcat9
+sudo systemctl enable nginx
+
+[ ! -d ~/works ] && mkdir ~/works
+cd ~/works
+
+echo '#### Download guacamole daemon and client war'
+[ ! -d ~/works/guacamole-server-${GUACAMOLE_VERSION} ] && \
+wget https://downloads.apache.org/guacamole/${GUACAMOLE_VERSION}/source/guacamole-server-${GUACAMOLE_VERSION}.tar.gz -O - | tar xzvf -
+[ ! -f ~/works/guacamole-${GUACAMOLE_VERSION}.war ] && \
+wget https://downloads.apache.org/guacamole/${GUACAMOLE_VERSION}/binary/guacamole-${GUACAMOLE_VERSION}.war
+
+if [ -d ~/works/guacamole-server-${GUACAMOLE_VERSION} ]; then
+cd ~/works/guacamole-server-${GUACAMOLE_VERSION}
+./configure --with-init-dir=/etc/init.d
+make
+sudo make install
+fi
+
+# refresh libs
+sudo ldconfig
+#sudo systemctl enable guacd
+
+# guacamole client
+[ ! -d ~/etc/guacamole ] && sudo mkdir -p /etc/guacamole
+sudo cp ~/works/guacamole-${GUACAMOLE_VERSION}.war /etc/guacamole/guacamole.war
+sudo ln -sf /etc/guacamole/guacamole.war /var/lib/tomcat9/webapps/
+
+sudo mkdir -p /etc/guacamole/{extensions,lib}
+sudo tee /etc/guacamole/guacamole.properties &>/dev/null <<EOF
+guacd-hostname: localhost
+guacd-port:    4822
+user-mapping:    /etc/guacamole/user-mapping.xml
+EOF
+
+#sudo echo "GUACAMOLE_HOME=/etc/guacamole" >> /etc/default/tomcat9
+
+sudo tee /etc/guacamole/user-mapping.xml &>/dev/null <<EOF
+<user-mapping>
+    <authorize
+            username="admin"
+            password="${ADMIN_PASSWORD}"
+            encoding="md5">
+        <connection name="default">
+            <protocol>rdp</protocol>
+            <param name="hostname">10.193.166.254</param>
+            <param name="port">3389</param>
+            <param name="username">ubuntu</param>
+            <param name="password">P@ssw0rd</param>
+        </connection>
+    </authorize>
+</user-mapping>
+EOF
+
+
+sudo tee /etc/nginx/sites-enabled/guacamole &>/dev/null <<EOF
+server {
+	listen 80;
+	server_name ${GUACAMOLE_FQDN};
+	
+	location / {
+		proxy_pass http://localhost:8080/guacamole/;
+		proxy_buffering off;
+		access_log off;
+		tcp_nodelay on;
+		tcp_nopush off;
+		sendfile on;
+		client_body_buffer_size 10K;
+		client_max_body_size 8m;
+		client_body_timeout 12;
+		keepalive_timeout 15;
+		send_timeout 10;
+		proxy_http_version 1.1;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+		proxy_set_header Upgrade \$http_upgrade;
+		proxy_set_header Connection \$http_connection;
+		proxy_cookie_path /guacamole/ /;
+	}
+}
+
+EOF
+
+#sudo ln -sf /etc/guacamole/nginx-site-guacamole /etc/nginx/sites-enabled/
+
+sudo certbot --nginx -d ${GUACAMOLE_FQDN} --agree-tos
+
+# enable crontab for renew certbot
+#sudo crontab -e
